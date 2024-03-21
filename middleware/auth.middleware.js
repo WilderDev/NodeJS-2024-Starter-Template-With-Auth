250feca7be066d63b791d3085f79b876660d4d13
WilderDev <williamjohnwilder@outlook.com>
2024-03-21 17:59:54 -0500

    AUTH MIDDLEWARE

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,56 +1,64 @@
 // * IMPORTS
 const { isTokenValid, attachCookies } = require("../lib/jwt");
 const { Token } = require("../models/Token.model");
+const { bad} = require('../lib/utils/res');
+
 
 // * MIDDLEWARE
-// Authenticate user
+// Authenticate User
 async function authenticateUser(req, res, next) {
-	const { refreshToken, accessToken } = req.signedCookies;
+	const { refreshToken, accessToken } = req.signedCookies; // Grab the cookies from the request
 
+	// Try to authenticate the user
 	try {
+		// If the access token is present, check if it's valid
 		if (accessToken) {
-			const payload = isTokenValid(accessToken);
-			req.user = payload.user;
-			return next();
+			const payload = isTokenValid(accessToken); // Check if the token is valid
+
+			req.user = payload.user; // Set the user in the request object so it can be accessed in the route
+
+			return next(); // Move on to the next middleware
 		}
+
+		// If the refresh token is present, check if it's valid
 		const payload = isTokenValid(refreshToken);
+
+		// If the refresh token is valid, attach the cookies and move on to the next middleware
 		const existingToken = await Token.findOne({
 			user: payload.user.userId,
 			refreshToken: payload.refreshToken
 		});
+
+		// If the token doesn't exist or isn't valid, send a 401 response
 		if (!existingToken || !existingToken?.isValid) {
-			res.status(401).json({
-				success: false,
-				data: {
-					message: "authentication invalid"
-				}
-			});
+			return bad({res, status: 401, message: "Unauthorized"})
 		} else {
+			// If the token is valid, attach the cookies and move on to the next middleware
 			attachCookies({
 				res,
 				user: payload.user,
 				refreshToken: existingToken.refreshToken
 			});
-			req.user = payload.user;
-			next();
+
+			req.user = payload.user; // Set the user in the request object so it can be accessed in the route
+
+			return next(); // Move on to the next middleware
 		}
 	} catch (err) {
-		res.status(500).send();
+		return bad({res, status: 500, message: "Server Error"})
 	}
 }
 
-// Authorize permissions
+// Authorize Permissions
 const authorizePermissions = (...roles) => {
+	// Return a middleware function
 	return (req, res, next) => {
+		// If the user's role isn't included in the roles array, send a 401 response
 		if (!roles.includes(req.user.role)) {
-			return res.status(401).json({
-				success: false,
-				data: {
-					message: "unauthorized to access this route"
-				}
-			});
+			return bad({res, status: 401, message: "Unauthorized"})
 		}
-		next();
+
+		next(); // Move on to the next middleware
 	};
 };
 
